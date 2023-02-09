174a671cbf9545fd77597a40a7f61131e49bcbe5
Blaxk <blaxkmaster@gmail.com>
2023-02-08 23:07:47 -0500

    feat: add pre-build script

--- install_fonts.py
+++ install_fonts.py
@@ -4,3 +4,13 @@ import urllib.request
 import zipfile
 import subprocess
 import glob
+
+# Pre-build
+if (not os.path.exists("./temp/font_files")):
+    os.mkdir("./temp/font_files")
+
+# .py files
+import __constants
+
+
+def run():
