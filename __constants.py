d3bb16def6d6166de914355e225f6dd2e57c852a
Blaxk <blaxkmaster@gmail.com>
2023-02-08 23:06:47 -0500

    feat: add all the font extension in an config file

--- __constants.py
+++ __constants.py
@@ -2,3 +2,5 @@ fontURL = [
     "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip",
     "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/DroidSansMono.zip"
 ]
+
+font_extension = ['ttf', "otf"]
