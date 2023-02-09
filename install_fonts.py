19cd9da7f0e05636fc9b4089cf0220ce4a5ccceb
Blaxk <blaxkmaster@gmail.com>
2023-02-08 23:08:27 -0500

    feat: add function to uncompress zip files

--- install_fonts.py
+++ install_fonts.py
@@ -26,3 +26,20 @@ def download_font(url):
     except:
         print(f'Failed to download: {fileName}')
 
+
+def uncompress_font_file(zip_path):
+    zip_name = os.path.basename(zip_path)
+    folder_name = zip_name.split(".")[0]
+    extract_path = "./temp/font_files/" + folder_name
+
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as z:
+            z.extractall(extract_path)
+
+        print(f'Successfully uncompressed file {zip_name} on folder {extract_path}')
+        extracted_files = getAllFiles(extract_path, __constants.font_extension)
+        
+        print('\n'.join('{}: {}'.format(*file) for file in enumerate(extracted_files)))
+        return extracted_files
+    except:
+        print(f'Failed to uncompress: {zip_path}')
