b3002cacd5a9431d24ece834b5a33b69b34812bf
Blaxk <blaxkmaster@gmail.com>
2023-02-08 23:08:08 -0500

    feat: add function to download fonts

--- install_fonts.py
+++ install_fonts.py
@@ -14,3 +14,15 @@ import __constants
 
 
 def run():
+def download_font(url):
+    fileName = os.path.basename(url) 
+
+    print(f'fileName = {url}')
+    try:
+        retrieve_response = urllib.request.urlretrieve(
+            url, f'./temp/font_files/{fileName}' )
+        file_path = retrieve_response[0]
+        return file_path
+    except:
+        print(f'Failed to download: {fileName}')
+
